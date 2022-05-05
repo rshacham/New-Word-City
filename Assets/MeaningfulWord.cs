8beaa29f46f94a8d42600fbdf5b9bd8cc904ae3e
Bar Nemesh <barnemesh@gmail.com>
2022-05-05 21:28:34 +0300

    Some Work on creating generic scripts to handle words

---
+++ Assets/MeaningfulWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "Word Meanings", menuName = "Word Meaning", order = 0)]
+    public class MeaningfulWord : ScriptableObject
+    {
+        [SerializeField]
+        private string word;
+
+        [SerializeField]
+        private List<MeaningDescriptor> meanings = new List<MeaningDescriptor>();
+
+        public string Word => word;
+
+        public List<MeaningDescriptor> Meanings => meanings;
+
+        public int MeaningFoundCount
+        {
+            get { return Meanings.Sum(descriptor => descriptor.Found ? 1 : 0); }
+        }
+    }
+
+    [Serializable]
+    public class MeaningDescriptor
+    {
+        [SerializeField]
+        [Multiline]
+        private string meaning;
+
+        public bool Found { get; set; } = false;
+        public string Meaning => meaning;
+    }
+}
