b9da7b215f5754ec5a11221046829f7dd229129e
Bar Nemesh <barnemesh@gmail.com>
2022-05-06 13:54:02 +0300

    found Meanings now persists with scene change

--- Assets/Mechanics/WordBase/WordsSceneManager.cs
+++ Assets/Mechanics/WordBase/WordsSceneManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,10 @@ namespace Mechanics.WordBase
 {
     public class WordsSceneManager : MonoBehaviour
     {
+        //TODO: index? scene object itself?
+        private static Dictionary<string, List<MeaningfulWord>> _savedScenesData =
+            new Dictionary<string, List<MeaningfulWord>>();
+
         #region Inspector
 
         [SerializeField]
@@ -33,11 +38,29 @@ namespace Mechanics.WordBase
         }
 
         #endregion
-        
+
         #region MonoBehaviour
 
         private void Awake()
         {
+            // TODO: check on default getters?
+            if (_savedScenesData.ContainsKey(SceneManager.GetActiveScene().name))
+            {
+                //TODO: move to MeaningfulWord as update method!
+                var saved = _savedScenesData[SceneManager.GetActiveScene().name];
+                for (int i = 0; i < words.Count; i++)
+                {
+                    for (int j = 0; j < words[i].Meanings.Count; j++)
+                    {
+                        words[i].Meanings[j].Found = saved[i].Meanings[j].Found;
+                    }
+                }
+            }
+            else
+            {
+                _savedScenesData[SceneManager.GetActiveScene().name] = words;
+            }
+
             WordsGameManager.Instance = this;
             WordsGameManager.SwitchToNextAvailableWord();
             // WordsGameManager.Current = words[0];
@@ -47,6 +70,7 @@ namespace Mechanics.WordBase
 
         private void OnDisable()
         {
+            _savedScenesData[SceneManager.GetActiveScene().name] = words;
             WordsGameManager.Instance = null;
         }
 
