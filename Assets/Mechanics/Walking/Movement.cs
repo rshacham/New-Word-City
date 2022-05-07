6d51f4074b5cad10028bec3b2d469e5d51eef0b2
rshacham <roi.shacham@mail.huji.ac.il>
2022-05-08 00:15:59 +0300

    Basic Keyboard Movement
    Just for working on the scene, Need to implement joystick movement

---
+++ Assets/Mechanics/Walking/Movement.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody;
+    private bool movingRight;
+    private bool movingLeft;
+    private bool movingUp;
+    private bool movingDown;
+
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float speedPerFrame;
+
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Vector2 oldVelocity = playerRigidBody.velocity;
+        //
+        // if (MovingRight)
+        // {
+        //     playerRigidBody.velocity = new Vector2(maxSpeed, oldVelocity.y);
+        // }
+        //
+        // else if (MovingLeft)
+        // {
+        //     playerRigidBody.velocity = new Vector2(-maxSpeed, oldVelocity.y);
+        // }
+        //
+        // else
+        // {
+        //     playerRigidBody.velocity = new Vector2(0f, oldVelocity.y);
+        // }
+        //
+        // if (movingUp)
+    }
+
+    public void MoveRight(InputAction.CallbackContext context)
+    {
+        Vector2 oldVelocity = playerRigidBody.velocity;
+        if (context.started && !movingLeft)
+        {
+            movingRight = true;
+        }
+
+        if (movingRight)
+        {
+            playerRigidBody.velocity = new Vector2(maxSpeed, oldVelocity.y);
+        }
+
+        if (context.canceled)
+        {
+            playerRigidBody.velocity = new Vector2(0, oldVelocity.y);
+            movingRight = false;
+        }
+    }
+    
+    public void MovevLeft(InputAction.CallbackContext context)
+    {
+        Vector2 oldVelocity = playerRigidBody.velocity;
+        if (context.started && !movingRight)
+        {
+            movingLeft = true;
+        }
+
+        if (movingLeft)
+        {
+            playerRigidBody.velocity = new Vector2(-maxSpeed, oldVelocity.y);
+        }
+
+        if (context.canceled)
+        {
+            playerRigidBody.velocity = new Vector2(0, oldVelocity.y);
+            movingLeft = false;
+        }
+    }
+    
+    public void MoveUp(InputAction.CallbackContext context)
+    {
+        Vector2 oldVelocity = playerRigidBody.velocity;
+        if (context.started && !movingDown)
+        {
+            movingUp = true;
+        }
+
+        if (movingUp)
+        {
+            playerRigidBody.velocity = new Vector2(oldVelocity.x, maxSpeed);
+        }
+
+        if (context.canceled)
+        {
+            playerRigidBody.velocity = new Vector2(oldVelocity.x, 0);
+            movingUp = false;
+        }
+    }
+    
+    public void MoveDown(InputAction.CallbackContext context)
+    {
+        Vector2 oldVelocity = playerRigidBody.velocity;
+        if (context.started && !movingUp)
+        {
+            movingDown = true;
+        }
+
+        if (movingDown)
+        {
+            playerRigidBody.velocity = new Vector2(oldVelocity.x, -maxSpeed);
+        }
+
+        if (context.canceled)
+        {
+            playerRigidBody.velocity = new Vector2(oldVelocity.x, 0);
+            movingDown = false;
+        }
+    }
+
+
+}
+
