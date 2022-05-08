b0469221786ac37a8b979b880417779254329c5c
Bar Nemesh <barnemesh@gmail.com>
2022-05-08 10:14:22 +0300

    Added Isometric Movement based on angle

--- Assets/Mechanics/Walking/Movement.cs
+++ Assets/Mechanics/Walking/Movement.cs
@@ -7,14 +7,37 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D playerRigidBody;
+
     private bool movingRight;
+
     private bool movingLeft;
+
     private bool movingUp;
+
     private bool movingDown;
 
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float speedPerFrame;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float speedPerFrame;
 
+    [SerializeField]
+    private bool moveIsometric;
+    
+    [SerializeField]
+    [Range(0, 180)]
+    private int isometricAngle = 120;
+
+    private Vector2 _isoVector;
+
+    private readonly Quaternion _moveAngle = Quaternion.Euler(0, 0, -45);
+
+
+    private void OnValidate()
+    {
+        var angle = Mathf.Deg2Rad * ((180 - isometricAngle) / 2f);
+        _isoVector = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
 
     private void Awake()
     {
@@ -40,7 +63,7 @@ public class Movement : MonoBehaviour
             movingRight = false;
         }
     }
-    
+
     public void MovevLeft(InputAction.CallbackContext context)
     {
         Vector2 oldVelocity = playerRigidBody.velocity;
@@ -60,7 +83,7 @@ public class Movement : MonoBehaviour
             movingLeft = false;
         }
     }
-    
+
     public void MoveUp(InputAction.CallbackContext context)
     {
         Vector2 oldVelocity = playerRigidBody.velocity;
@@ -80,7 +103,7 @@ public class Movement : MonoBehaviour
             movingUp = false;
         }
     }
-    
+
     public void MoveDown(InputAction.CallbackContext context)
     {
         Vector2 oldVelocity = playerRigidBody.velocity;
@@ -101,6 +124,25 @@ public class Movement : MonoBehaviour
         }
     }
 
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        var movementVector = context.action.ReadValue<Vector2>();
+        if (moveIsometric)
+        {
+            movementVector = _moveAngle * movementVector;
+        }
 
+        playerRigidBody.velocity = movementVector * _isoVector * maxSpeed;
+    }
+
+    public void OnMovementUnNormalized(InputAction.CallbackContext context)
+    {
+        // var movementVector = context.action.ReadValue<Vector2>();
+        // if (moveIsometric)
+        // {
+        //     // TODO: check if should normalize or not based on feedback
+        //     movementVector = _moveAngle * movementVector.normalized;
+        // }
+        // playerRigidBody.velocity = movementVector * _isoVector * maxSpeed;
+    }
 }
-
