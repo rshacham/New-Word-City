763668d26c3c985890462fe986dc2851f899c1e7
Bar Nemesh <barnemesh@gmail.com>
2022-05-11 13:01:17 +0300

    Push mechanic tested- should probably use animation instead

---
+++ Assets/Mechanics/Object Interactions/InteractionScripts/SeparateInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Avrahamy;
+using UnityEngine;
+
+namespace Mechanics.Object_Interactions.InteractionScripts
+{
+    class SeparateInteractable : EventInteractable
+    {
+        [Space(2)]
+        [Header("Separated")]
+        [SerializeField]
+        private InteractableObject from;
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Interactable") && other.gameObject == from.gameObject)
+            {
+                DebugLog.Log("Separated from", Color.white, from);
+                Interact();
+            }
+        }
+    }
+}
