00ac8c598858c97c99eff08fb427b0aec19e6d43
Bar Nemesh <barnemesh@gmail.com>
2022-05-16 20:32:56 +0300

    Transparency in TallBuilding.cs is now fade

--- Assets/Mechanics/A Combined Scene/TallBuilding.cs
+++ Assets/Mechanics/A Combined Scene/TallBuilding.cs
@@ -6,6 +6,8 @@ using UnityEngine.Serialization;
 
 public class TallBuilding : MonoBehaviour
 {
+    #region Inspector
+
     [Header("Hide Tall Objects")]
     [SerializeField]
     private Collider2D transparencyTrigger;
@@ -17,7 +19,25 @@ public class TallBuilding : MonoBehaviour
     [Range(0, 1)]
     private float transparency = 0.5f;
 
-    private Color _oldValue;
+    [SerializeField]
+    private float fadeTime = 0.5f;
+
+    #endregion
+
+    #region Private Fields
+
+    private float _t;
+    private int _direction = 1;
+    private bool _notActive = true;
+    private Color _normalColor;
+    private Color _fadeColor;
+
+    private Color _ColorA;
+    private Color _ColorB;
+
+    #endregion
+
+    #region MonoBehaviour
 
     private void Awake()
     {
@@ -31,22 +51,43 @@ public class TallBuilding : MonoBehaviour
             transparencyTrigger = GetComponent<Collider2D>();
             transparencyTrigger.isTrigger = true;
         }
+
+        _normalColor = mySprite.color;
+        _fadeColor = new Color(_normalColor.r, _normalColor.g, _normalColor.b, transparency);
+    }
+
+    private void Update()
+    {
+        if (_notActive)
+        {
+            return;
+        }
+
+        _t += _direction * Time.deltaTime / fadeTime;
+        _t = Mathf.Clamp(_t, 0, 1);
+        mySprite.color = Color.Lerp(_normalColor, _fadeColor, _t);
+        _notActive = _t >= 1 || _t <= 0;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            _oldValue = mySprite.color;
-            mySprite.color = new Color(_oldValue.r, _oldValue.g, _oldValue.b, transparency);
+            // (_ColorA, _ColorB) = (_normalColor, _fadeColor);
+            _direction = 1;
+            _notActive = false;
         }
     }
-    
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            mySprite.color = _oldValue;
+            // (_ColorA, _ColorB) = (_fadeColor, _normalColor);
+            _direction = -1;
+            _notActive = false;
         }
     }
+
+    #endregion
 }
