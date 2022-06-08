e9dc4b2cf4c1682a27ea80b3a3ee49263cff958a
Bar Nemesh <barnemesh@gmail.com>
2022-06-08 18:09:05 +0300

    Bus Move

--- Assets/Mechanics/Honk Interaction/HonkInteraction.cs
+++ Assets/Mechanics/Honk Interaction/HonkInteraction.cs
@@ -8,23 +8,27 @@ public class HonkInteraction : EventInteractable
 {
     #region Inspector
 
-    [SerializeField] private AudioSource firstSound;
-    [SerializeField] private AudioSource secondSound;
-    
+    [SerializeField]
+    private AudioSource firstSound;
+    [SerializeField]
+    private AudioSource secondSound;
+
     #endregion
-    
-    
+
+
     #region Private Fields
 
     private static int counter;
-    
+
     #endregion
+
     protected override void ScriptInteract()
     {
         if (firstSound.isPlaying && secondSound.isPlaying)
         {
             return;
         }
+
         DebugLog.Log(LogTag.Audio, "HONKS", this);
         switch (++counter % 2)
         {
