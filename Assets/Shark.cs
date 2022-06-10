328f80c28fc0f40282925daf8480cad978cd3277
rshacham <roi.shacham@mail.huji.ac.il>
2022-06-10 03:16:04 +0300

    Polished beach area + shark interaction

---
+++ Assets/Shark.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Interactable_Objects
+{
+    public class Shark : EventInteractable
+    {
+        #region Inspector
+
+        [SerializeField]
+        private Animator sharkAnimator;
+
+
+
+        #endregion
+        
+        #region Private Fields
+
+        private Animator _tubeAnimator;
+        
+        private Beach _beach;
+
+        #endregion
+        
+        
+        #region EventInteractable
+
+        private void Start()
+        {
+            _tubeAnimator = GetComponent<Animator>();
+            _beach = FindObjectOfType<Beach>();
+        }
+
+        #endregion
+
+
+        protected override void ScriptInteract()
+        {
+            sharkAnimator.SetTrigger("Movement");
+            sharkAnimator.SetBool("Animation", true);
+            _beach.ChangeSound(1);
+        }
+
+
+        public void StopInteraction()
+        {
+            _beach.ChangeSound(0);
+            sharkAnimator.SetBool("Animation", false);
+        }
+
+
+        public void CloseToTube(bool isClose)
+        {
+            _tubeAnimator.SetBool("Close", isClose);
+        }
+
+    }
+}
+
