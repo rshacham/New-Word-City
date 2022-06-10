db9a2919fe9aae7de11e53e290eff0065779a1a1
Bar Nemesh <barnemesh@gmail.com>
2022-06-10 20:38:49 +0300

    Added version of the random animation start time that uses state machine instead of monobehaviour

---
+++ Assets/SetTriggerAfterRandomTimeRange.cs
@@ -0,0 +1,52 @@
+using Avrahamy;
+using Avrahamy.Math;
+using BitStrap;
+using UnityEngine;
+using FloatRange = Avrahamy.Math.FloatRange;
+
+public class SetTriggerAfterRandomTimeRange : StateMachineBehaviour
+{
+    #region Inspector
+
+    [SerializeField]
+    [Tooltip("The trigger to move into the initial animation state")]
+    private TriggerAnimationParameter startTrigger;
+
+    [SerializeField]
+    [Tooltip("Time range to start in")]
+    private FloatRange range;
+
+    #endregion
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var delay = RandomUtils.Range(range);
+        PureCoroutines.DelaySeconds(() => startTrigger.Set(animator), delay);
+        // DebugLog.Log(LogTag.LowPriority, $"delay: {delay}", this);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
